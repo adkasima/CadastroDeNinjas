8ba846a2af2d20525ef6d77aa4dcfd40a49bb35f
Adriel <adrielkasima@gmail.com>
2025-02-24 11:12:25 -0300

    feat: Update NinjaController with data mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,43 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
     @GetMapping("/hello")
-    public String olaMundo() {
+    public String hello() {
         return "Olá mundo!";
+
+    }
+
+    //Cadastrar Ninjas
+    @PostMapping("/cadastrar")
+    public String registerNinja() {
+        return "Cadastrar ninja";
+    }
+
+    //Mostrar todos os ninjas
+    @GetMapping("/mostrar")
+    public String showNinjas() {
+        return "mostrar ninjas";
+    }
+
+    //Monstrar Ninja por ID
+    @GetMapping("/mostrarID")
+    public String showNinjaByID() {
+        return "mostrar ninjas por ID";
+    }
+
+    //Alterar dados do ninja
+    @PutMapping("/alterar")
+    public String updateNinjaByID() {
+        return "alterar dados por ID";
+    }
+    //Deletar Ninja
+    @DeleteMapping("/deletar")
+    public String deleteNinjaByID() {
+        return "deletar ninja por ID";
     }
 }
