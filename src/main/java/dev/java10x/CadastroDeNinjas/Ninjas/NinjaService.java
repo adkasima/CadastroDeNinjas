a8765296acb4f40b2e24559090e81c2cd934c5d5
Adriel <adrielkasima@gmail.com>
2025-02-27 19:33:44 -0300

    feat: Create NinjaService

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,19 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private final NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    public List<NinjaModel> listNinjas() {
+        return ninjaRepository.findAll();
+    }
 }
